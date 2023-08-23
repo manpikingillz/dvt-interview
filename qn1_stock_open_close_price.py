bc0d2f97df9b77c09a101e35eb3e4cd11038a32e
Gilbert Twesigomwe <gilbert@bposeats.com>
2023-08-23 14:24:37 +0300

    naive solution to question 1 - on stocks

--- qn1_stock_open_close_price.py
+++ qn1_stock_open_close_price.py
@@ -45,3 +45,34 @@ Sample Output:
     7-January-2000 5358.28 5414.48
     11-January-2000 5513.04 5296.3
 '''
+import datetime
+import requests
+
+
+def openAndClosePrices(firstDate, lastDate):
+    first_date = datetime.datetime.strptime(firstDate, '%d-%B-%Y')
+    last_date = datetime.datetime.strptime(lastDate, '%d-%B-%Y')
+
+    current_date = first_date
+
+    while current_date <= last_date:
+        date = current_date.strftime('%d-%B-%Y')
+        response = requests.get(
+            f'https://jsonmock.hackerrank.com/api/stocks?date={date}')
+        current_date += datetime.timedelta(days=1)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            for item in data['data']:
+                items = [item['date'], str(item['open']), str(item['close'])]
+                print('   '.join(items))
+        else:
+            print("Error Occured: ", response.status_code)
+
+
+if __name__ == "__main__":
+    first_date = "8-March-2002"
+    last_date = "15-March-2002"
+
+    openAndClosePrices(first_date, last_date)
