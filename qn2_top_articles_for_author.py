aa2240e58dca21bf14dcaa98d4f387c6d405f311
Gilbert Twesigomwe <gilbert@bposeats.com>
2023-08-23 15:39:48 +0300

    add naive solution for question 2 - top articles for author

---
+++ qn2_top_articles_for_author.py
@@ -0,0 +1,32 @@
+# Given username and limit values, return an array of names of articles authored by the user with the given username
+# orderd by the number of comments they have, descedning. limit the list to limit records.
+# If articles have the same number of comments, order them by name asc
+
+import requests
+
+
+def topArticles(username, limit):
+    response = requests.get(
+        f'https://jsonmock.hackerrank.com/api/articles?author={username}&limit={limit}')
+
+    if response.status_code == 200:
+        data = response.json()
+
+        articles_data = data['data']
+        sorted_articles = sorted(
+            articles_data,
+            key=lambda article: (-article['num_comments'], article['title']))
+
+        article_names = []
+        for item in sorted_articles:
+            article_names.append(item['title'])
+        return article_names
+    else:
+        print("Error Occured: ", response.status_code)
+
+
+if __name__ == "__main__":
+    username = "epaga"
+    limit = 5
+
+    print('article names: ', topArticles(username, limit))
